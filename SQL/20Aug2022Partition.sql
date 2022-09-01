10f39125e21434a13cd94eee55535a4b274336a4
Raghvendra Yadav <43167824+RaghvendraYadav@users.noreply.github.com>
2022-09-01 23:56:23 +0530

    Add files via upload

---
+++ SQL/20Aug2022Partition.sql
@@ -0,0 +1,269 @@
+create database Ineuron_Partition;
+use ineuron_partition;
+
+Create table ineuron_course(
+course_name varchar(50),
+course_id int,
+course_title varchar(60),
+course_desc varchar(60),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int
+);
+
+select * from ineuron_course;
+
+insert into ineuron_course values("machine_learning",101,"ML","This is ML Course",'2019-07-07',3540,'Sudhanshu',2019),
+('aiops' , 101 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+
+select * from ineuron_course where course_launch_year=2020;
+
+# Seprate the Data and lok into the Specific Record
+# Store a data internally Year Wise
+# Partition using Range
+Create table ineuron_course1(
+course_name varchar(50),
+course_id int,
+course_title varchar(60),
+course_desc varchar(60),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by range(course_launch_year)
+(
+partition  p0 values less than (2019),
+partition  p1 values less than (2020),
+partition  p2 values less than (2021),
+partition  p3 values less than (2022),
+partition  p4 values less than (2023)
+);
+drop table ineuron_course1;
+insert into ineuron_course1 values("machine_learning",101,"ML","This is ML Course",'2019-07-07',3540,'Sudhanshu',2019),
+('aiops' , 101 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+
+select * from ineuron_course1 where course_launch_year=2020;# partition Takes less time 
+select * from ineuron_course where course_launch_year=2020;# this take more time to execute
+
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course1';
+# Partition Using hash
+Create table ineuron_course2(
+course_name varchar(50),
+course_id int,
+course_title varchar(60),
+course_desc varchar(60),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by hash(course_launch_year)
+partitions 5;
+
+#Checking the Partition
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course2';
+
+
+insert into ineuron_course2 values("machine_learning",101,"ML","This is ML Course",'2019-07-07',3540,'Sudhanshu',2019),
+('aiops' , 101 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+#Checking the Partition
+
+# Partition is Done on Year column 2019%5= 2020 %5 so the remainder we get it will go there only 
+# Hash Partition means Year%total no of partition then it will go to the remainder indexx 2020%5 =0 so it will go to P0 partition
+
+# Key Partition  it also use another Hashing Algorithm MD5()
+Create table ineuron_course4(
+course_name varchar(50),
+course_id int primary key ,
+course_title varchar(60),
+course_desc varchar(60),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by key()
+partitions 5;
+
+#Checking the Partition
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course4';
+insert into ineuron_course4 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('aiops' , 102 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 103 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 104 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 105, 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 106 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 107 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 108 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 109 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 110 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 1011 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 1012 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 1013 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 1014 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 1015 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+#Checking the  Key Partition
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course4';
+
+
+# List Partition 
+
+Create table ineuron_course6(
+course_name varchar(50),
+course_id int ,
+course_title varchar(60),
+course_desc varchar(60),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by list(course_launch_year)
+(partition p0 values in (2019,2020),
+partition p1 values in (2021),
+partition p2 values in (2022));
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course4';
+insert into ineuron_course6 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('aiops' , 102 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 103 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 104 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 105, 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 106 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 107 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 108 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 109 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 110 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 1011 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 1012 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 1013 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 1014 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 1015 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+#Checking the  List  Partition
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course6';
+
+# Range Column Partition 
+
+Create table ineuron_course7(
+course_name varchar(50),
+course_id int ,
+course_title varchar(60),
+course_desc varchar(60),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by range columns(course_name,course_id,course_launch_year)
+(partition p0 values less than ('aiops',105,2019),
+partition p1 values less than ('fsds' ,110,2021),
+partition p2 values less than ('MERN',116,2023)
+);
+
+insert ignore into ineuron_course7 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('aiops' , 102 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 103 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 104 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 105, 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 106 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 107 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 108 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 109 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 110 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 1011 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 1012 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 1013 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 1014 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 1015 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+
+#Checking the  List  Partition
+select partition_name,table_name,table_rows from information_schema.partitions where table_name='ineuron_course7';
+
+# List Column Partitiob
+
+Create table ineuron_courses9(
+course_name varchar(50) ,
+course_id int(10) ,
+course_title varchar(60),
+course_desc varchar(80),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by list columns(course_name)(
+partition p0 values  in('aiops','data analytics','Dl','RL'),
+partition p1 values  in('fsds' ,'big data','blockchain'),
+partition p2 values  in('MERN','java','interview prep','fsda')
+);
+
+
+insert ignore into ineuron_courses9 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+
+('aiops' , 102 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('dlcvnlp' , 103 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('aws cloud' , 104 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('blockchain' , 105, 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('RL' , 106 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('Dl' , 107 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('interview prep' , 108 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+('big data' , 109 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('data analytics' , 110 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fsds' , 1011 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('fsda' , 1012 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+('fabe' , 1013 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+('java' , 1014 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+('MERN' , 1015 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019);
+
+
+create table ineuron_courses10(
+course_name varchar(50),
+course_id int(10),
+course_title varchar(60),
+course_desc varchar(80),
+launch_date date,
+course_fee int,
+course_mentor varchar(60),
+course_launch_year int)
+partition by range(course_launch_year)
+subpartition by hash(course_launch_year)
+subpartitions 5 (
+partition p0 values less than (2019) ,
+partition p1 values less than (2020) ,
+partition p2 values less than (2021) ,
+partition p3 values less than (2022) 
+);
